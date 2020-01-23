096948d04c43402fd73a2e470489e3c1ac7f9b4c
nitrid <alikemalkaraca@gmail.com>
2020-01-23 13:43:43 +0300

    .

--- sql/license.js
+++ sql/license.js
@@ -1,39 +1,44 @@
-var MacId = require('node-machine-id');
-var fs = require('fs');
+let MacId = require('node-machine-id');
+let fs = require('fs');
 
-var LicIo = require('socket.io-client');
-var LicSoc = LicIo.connect('http://licence.teknoticari.com:8090',{'timeout':2000, 'connect timeout': 2000});
+let LicIo = require('socket.io-client');
+let LicSoc = LicIo.connect('http://licence.teknoticari.com:8090',{'timeout':2000, 'connect timeout': 2000});
+//let LicSoc = LicIo.connect('http://localhost:8090',{'timeout':2000, 'connect timeout': 2000});
+let AppId = "02";
 
 function LicenseCheck(callback)
 {
     if(typeof callback != 'undefined')
     {
-        LicSoc.on('connect', function () 
-        {   
-            console.log('MacId : ' + MacId.machineIdSync());
-            
-            LicSoc.emit('licensecheck',{MacId:MacId.machineIdSync()},function(data)
+        if(LicSoc.connected)
+        {
+            LicSoc.emit('licensecheck',{MacId: AppId + MacId.machineIdSync()},function(data)
             {
                 WriteLic(data);
-
-                if(data.result.length > 0)
-                {                    
-                    callback(data);
-                }
-                else
-                {
-                    callback(data);
-                }
-            });            
-        });
-        LicSoc.on('connect_error', function (socket) 
-        {
-            ReadLic(function(data)
-            {
-                LicSoc.close();
                 callback(data);
-            })
-        });
+            });     
+        }
+        else
+        {
+            LicSoc.on('connect', function () 
+            {   
+                console.log('MacId : ' + AppId + MacId.machineIdSync());
+                LicSoc.emit('licensecheck',{MacId: AppId + MacId.machineIdSync()},function(data)
+                {
+                    WriteLic(data);
+                    callback(data);
+                });            
+            });
+            LicSoc.on('connect_error', function (socket) 
+            {
+                ReadLic(function(data)
+                {
+                    LicSoc.close();
+                    callback(data);
+                })
+            });
+        }
+        
     }
 }
 function WriteLic(pData)
